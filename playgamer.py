7cd1285aa244b955980c922e8f1017f67ceebfde
mielesantos <mielesnts@gmail.com>
2020-05-10 16:31:34 -0300

    reorganizando arquivos

---
+++ playgamer.py
@@ -0,0 +1,19 @@
+from gamers import forca
+from gamers import  adivinhacao
+
+print("*********************************")
+print("Informe o numero para opção do jogo!")
+print("*********************************")
+
+print("\n(1) Forca\n(2) Adivinhação!")
+
+jogo = int(input("Qual jogo dejesa joga? "))
+
+#Definindo qual jogo foi selecionado e executando
+if(jogo == 1):
+    print("Iniciando o jogo da Forca")
+    forca.jogar()
+
+elif(jogo == 2):
+    print("Iniciando o jogo da adivinhação")
+    adivinhacao.jogar()

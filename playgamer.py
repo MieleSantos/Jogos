23487da7e38e2f219a01aecf88043ba1bf2edc68
derpy <mielesnts@gmail.com>
2020-06-01 20:33:18 -0300

    Update playgamer.py

--- playgamer.py
+++ playgamer.py
@@ -1,11 +1,13 @@
 from gamers import forca
-from gamers import  adivinhacao
+#from gamers import  adivinhacao
 
 print("*********************************")
 print("Informe o numero para opção do jogo!")
 print("*********************************")
 
-print("\n(1) Forca\n(2) Adivinhação!")
+print("\n(1) Adivinhação!")
+
+# print("\n(1) Forca\n(2) Adivinhação!")
 
 jogo = int(input("Qual jogo dejesa joga? "))
 
@@ -14,6 +16,6 @@ if(jogo == 1):
     print("Iniciando o jogo da Forca")
     forca.jogar()
 
-elif(jogo == 2):
-    print("Iniciando o jogo da adivinhação")
-    adivinhacao.jogar()
+# elif(jogo == 2):
+#     print("Iniciando o jogo da adivinhação")
+#     adivinhacao.jogar()
